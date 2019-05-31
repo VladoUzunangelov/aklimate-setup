dc64cc4f2222ece23dfdd7e381a1679dc07aa9a6
chrisw <chrisw@soe.ucsc.edu>
2019-05-31 16:13:51 -0700

    add RMySQL R package

--- dockerfile_scripts/install_r_stuff.sh
+++ dockerfile_scripts/install_r_stuff.sh
@@ -3,7 +3,7 @@
 #chrisw 20190520
 #install various R packages for running AKLIMATE
 
-R -e 'install.packages(c("foreach","doParallel","ranger","plyr","abind","ROCR","caret","proxy","purrr","pracma","fastmatch","devtools","mlr","e1071","igraph","circlize","RColorBrewer"))'
+R -e 'install.packages(c("foreach","doParallel","ranger","plyr","abind","ROCR","caret","proxy","purrr","pracma","fastmatch","devtools","mlr","e1071","igraph","circlize","RColorBrewer","RMySQL"))'
 R -e 'source("https://bioconductor.org/biocLite.R")'
 R -e 'BiocInstaller::biocLite(c("ComplexHeatmap","FDb.InfiniumMethylation.hg19"))'
 R -e 'devtools::install_github("VladoUzunangelov/similarity")'
