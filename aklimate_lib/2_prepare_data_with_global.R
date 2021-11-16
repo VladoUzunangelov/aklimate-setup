7d77e230715724b53d33aecb5cf3885b962bffb2
chrisw <chrisw@soe.ucsc.edu>
2021-11-16 11:56:31 -0800

    version of script to use global features

---
+++ aklimate_lib/2_prepare_data_with_global.R
@@ -0,0 +1,317 @@
+#!/usr/bin/env Rscript
+# vuzunangelov & chrisw 20190606
+
+## Run the commands in this script after those in 1_load_aklimate_libs.R.
+
+QUANTIZE_NUMERIC_DATA = FALSE
+USE_GLOBAL = TRUE
+
+homeDir <- "./p_store_files"
+workDir <- "./models"
+
+message("load sample data")
+
+## MIR is also there - about 800 MIRs right now probably best to not include them
+## - I have a scheme to indluce them by putting a MIR in every pathway that has
+## one of its targets, but it didn't work well when I tested it
+nomir = c("MUTA", "CNVR", "METH", "GEXP")
+mir = c("MUTA", "CNVR", "METH", "GEXP","MIR")
+suffs <- list(mir)
+
+dat <- read.delim("./data/combined_matrix.tsv", header = T, row.names = 1, check.names = FALSE)
+dat <- dat[, -1]
+
+
+message("meth mapper")
+
+saved_meth_mapper_file <- "meth_mapper_hm450_20200416.RData"
+found_meth_mapper_file <- TRUE %in% (list.files(path=homeDir) == saved_meth_mapper_file)
+
+saved_meth_mapper_file <- paste0(homeDir, "/meth_mapper_hm450_20200416.RData")
+if (found_meth_mapper_file) {
+  message(paste0("found ", saved_meth_mapper_file))
+} else {
+  message(paste0("getting a new meth.mapper to save to "), saved_meth_mapper_file)
+  hm450 <- get450k()
+  meth.mapper <- getNearestGene(hm450)
+  save(meth.mapper, file = saved_meth_mapper_file)
+}
+
+message(paste0("loading meth.mapper from "), saved_meth_mapper_file)
+load(saved_meth_mapper_file)
+
+updated.names <- foreach(i = iter(colnames(dat)), .combine = c) %dopar% {
+  if (grepl("METH", i, ignore.case = TRUE)) {
+    cg <- strsplit(i, ":")[[1]][4]
+    gsub(paste0(cg, "::"), paste0(cg, ":", meth.mapper[cg, 4], ":"), i)
+  } else {
+    i
+  }
+}
+
+colnames(dat) <- updated.names
+
+
+message("filter out low expression features")
+# Theo has named all gene expression features with 'N:GEXP:'
+
+sampleIDs <- rownames(dat)
+exp_matrix <- dat[sampleIDs, grepl(paste0("N:GEXP:"), colnames(dat))]
+
+message(paste("number of features in input expression matrix", length(colnames(exp_matrix))))
+
+exp_means <- colMeans(exp_matrix)
+exp_sds <- apply(exp_matrix, 2, sd)
+
+## exclude genes that are in the bottom quartile by mean or sd
+exp_features_upper75_by_mean = names(exp_means)[exp_means > quantile(exp_means, probs = 0.25)]
+exp_features_upper75_by_sd = names(exp_sds)[exp_sds > quantile(exp_sds, probs = 0.25)]
+exp_all_features <- colnames(exp_matrix)
+exp_keep_features <- intersect(exp_features_upper75_by_mean, exp_features_upper75_by_sd)
+exp_drop_features <- setdiff(exp_all_features, exp_keep_features)
+
+full_keep_features <- setdiff(colnames(dat), exp_drop_features)
+
+message(paste("number of expression features to drop", length(exp_drop_features)))
+
+exp_matrix <- NULL
+
+message(paste("number of features in full data matrix", length(colnames(dat))))
+dat <- dat[sampleIDs, full_keep_features]
+message(paste("number of features in new data matrix", length(colnames(dat))))
+
+
+if (QUANTIZE_NUMERIC_DATA) {
+  message("quantize numeric datatypes")
+
+  dat <- foreach(datatype_str = iter(mir[!grepl("MUTA|CNVR", nomir)]), .combine = cbind) %do%
+    {
+      message(paste("quantize ", datatype_str))
+      quantize.data(dat[sampleIDs, grepl(paste0("N:", datatype_str, ":"), colnames(dat))],
+        nbr = 5, idx = sampleIDs)
+    }
+}
+
+# aklimate expects a dataframe, not a matrix
+dat <- as.data.frame(dat)
+
+if (USE_GLOBAL==TRUE) {
+	global_matrix <- dat[sampleIDs, grepl(paste0("GLOBAL:"), colnames(dat))]
+	global_feature_list <- colnames(global_matrix)
+}
+
+message(paste0("global_features: ", length(global_feature_list)))
+message(global_feature_list)
+
+message("load pathways")
+
+# p1 <- readSetList(paste0(homeDir, '/pathcomm_pathways_cleaned')) p1 <-
+# p1[!grepl('[[:digit:]]_SMPDB$|Z_SMPDB$', names(p1))]
+
+# using individual pathway files
+# p1 <- readSetList(paste0(homeDir, "/pathcomm_pathways_cleaned_non_redundant_names.tsv"))
+# p2 <- readSetList(paste0(homeDir, "/genomic_position_sets.listt"))
+# p3 <- readSetList(paste0(homeDir, "/genesigdb_human.tab"))
+# p4 <- readSetList(paste0(homeDir, "/msigdb_c2_c5_no_c2_cp.tab"))
+#
+# pathways <- c(p1, p2, p3, p4)
+
+# using one file of collected pathways.
+# The names of these pathways have been mapped to simple names.
+# This was done to avoid the continued problem of name collisions and the like.
+# collected_pathways_name_mapped.tsv
+p_collected <- readSetList(paste0(homeDir, "/collected_pathways_name_mapped.tsv"))
+pathways <- c(p_collected)
+
+max_size_of_pathways <- 1000
+pathways <- pathways[sapply(pathways, length) < max_size_of_pathways]
+
+# need to load the name mappings in order to map back to original pathway names
+p_name_mapping <- read.csv(file=paste0(homeDir, "/pathway_name_mapping.tsv"), header = FALSE, sep = "\t", col.names = c("p_id", "p_name"))
+
+message("sanitize pathway names")
+
+## probably a good idea to sanitize the name a bit
+names(pathways) <- gsub("[^\\w\\s]", "_", names(pathways), perl = TRUE)
+
+message(paste("number of pathways to use", length(names(pathways))))
+
+message("load labels")
+
+labels <- as.matrix(read.delim("./labels.tsv", check.names = F, stringsAsFactors = F,
+  header = F, row.names = 1))
+labels <- factor(labels[, 1])
+
+num_labels <- length(levels(labels))
+if (num_labels == 2) {
+  CLASSIFICATION_TYPE = "binary"
+} else if (num_labels > 2) {
+  CLASSIFICATION_TYPE = "multiclass"
+} else {
+  message(paste0("ERROR: detected ", num_labels, " labels"))
+  stopifnot(FALSE)
+}
+
+message(paste0("Detected ", num_labels, " labels. Setting CLASSIFICATION_TYPE to ", CLASSIFICATION_TYPE))
+
+message("load CV fold splits")
+
+splits <- as.matrix(read.delim("./data/cv_folds.tsv", check.names = F, stringsAsFactors = F,
+  header = TRUE, row.names = 1))
+splits <- splits[, -1]
+
+
+message("set tasks and seeds")
+
+tasks <- colnames(splits)
+
+# you can change the seeds - this is just to match initial runs
+seeds <- 11 * (1:length(tasks))
+names(seeds) <- tasks
+
+
+
+message("define worker.f")
+
+worker.f <- function(tasks) {
+
+  res <- foreach(i = iter(tasks)) %do% {
+
+    stats_preds_file_name = paste0(i, "_junkle_final_model_stats_preds.RData")
+    stats_preds_file_path = paste0(workDir, "/", stats_preds_file_name)
+
+    if (TRUE %in% (list.files(path=workDir) == stats_preds_file_name)) {
+      message(paste0("loading stats_preds from file: ", stats_preds_file_path))
+      load(stats_preds_file_path)
+    } else {
+      set.seed(seeds[i], kind = "L'Ecuyer-CMRG")
+
+      # description of AKLIMATE params is available at: https://github.com/VladoUzunangelov/aklimate
+
+      idx.train <- rownames(splits)[splits[, i] == 0]
+      idx.test <- setdiff(rownames(splits), idx.train)
+
+    	# Set nfold based on the size of the smallest class.
+    	# We encountered situations where the nfold was larger than the size of smallest class.
+    	# Best solution is to remove the tiny class, but here we attempt to keep all classes.
+    	default_nfold <- 5
+      num_folds <- default_nfold
+      for (class in levels(labels)) {
+        training_labels <- labels[idx.train]
+        training_set_for_class <- (training_labels)[training_labels[] == class]
+        num_folds <- min(length(training_set_for_class), num_folds)
+      }
+      message("setting num_folds = ", num_folds)
+
+
+    # classification_type <- "binary"
+    # classification_type <- "multiclass"
+      classification_type <- CLASSIFICATION_TYPE
+      message(paste0(i, " classification_type: ", classification_type))
+
+
+      message(paste0(i, " set params for junkle model"))
+      # dat, dat.grp, lbls, fsets, always.add=NULL, rf.pars=list(), junkle.pars=list(),
+      # store.kernels=FALSE, verbose=FALSE
+      junkle_training_data <- dat[idx.train, ]
+      junkle_datatypes <- suffs
+      junkle_training_labels <- labels[idx.train]
+      junkle_feature_sets <- pathways
+      #junkle_always_add <- sprintf("N:MIRSCORE::FAM_%s:", 0:29)
+      junkle_always_add <- global_feature_list
+
+      rf_params <- list(ttype = classification_type, bin.perf = c("bacc"), importance = "permutation",
+        min.nfeat = 15, ntree = 1000, sample.frac = 0.5, replace = FALSE, weights = NULL,
+        oob.cv = data.frame(min.node.prop = 0.01, mtry.prop = 0.25, ntree = 500))
+
+      # common warning message:
+      # The duality gap has been closing very slowly indicating slow convergence.You should examine your kernels for multicollinearity and or change regularization parameters.Alternatively you can increase minIter or decrease tolMinIter.
+
+      # MKL solver is converging very slowly.
+      # address this by using different range of regularization parameter tuning as below.
+      # The values are log2 scale, so negative values means very close to zero, i.e. low regularization
+
+      # small number underflow with rcpp might cause error like this:
+      # Error in { : task 8 failed - "NA/NaN argument"
+      # and also many slow convergence messages
+      # changing lamb range may correct this
+      junkle_lamb = c(-5, 0)
+      junkle_lamb = c(-15, 0)
+
+      # this is the setting used for TMP
+      junkle_lamb = c(-8, 3)
+
+      message("using lamb:")
+      message(paste0(" ", junkle_lamb))
+
+      junkle_params <- list(topn = 5, nfold = num_folds, subsetCV = TRUE, lamb = junkle_lamb, cvlen = 200, type = "probability")
+
+      junkle_store_kernels <- FALSE
+      junkle_verbose <- TRUE
+
+
+      message(paste0(i, " train model"))
+      # train AKLIMATE model
+      # works with Spicer.R with md5sum: 71b8fbb55747b623d565960ff7737f73
+      # does not work with Spicer.R with md5sum: c1fffbc913a9a82571d6a00116c6dadd
+      # jklm <- junkle(dat[idx.train, ], suffs, labels[idx.train], pathways, NULL, rf_params,
+      #   junkle_params, FALSE, TRUE)
+
+      model_filename = paste0(i, "_junkle_final_model.RData")
+      model_file_path = paste0(workDir, "/", model_filename)
+
+      if (TRUE %in% (list.files(path=workDir) == model_filename)) {
+        message(paste0("loading model from file: ", model_file_path))
+        load(model_file_path)
+      } else {
+        jklm <- junkle(junkle_training_data, junkle_datatypes, junkle_training_labels, junkle_feature_sets,
+          junkle_always_add, rf_params, junkle_params, junkle_store_kernels, junkle_verbose)
+
+        message(paste0("model training completed. saving model to file: ", model_file_path))
+        save(jklm, file = model_file_path)
+      }
+
+
+      message(paste0(i, " set params for predictions"))
+      # jklobj, dat, fsets, kernels=NULL, store.kernels=FALSE
+      # junkle_object <- jklm
+      junkle_predict_data <- dat[c(idx.train, idx.test), ]
+      # junkle_feature_sets <- pathways
+      junkle_kernels <- NULL
+      # junkle_store_kernels <- FALSE
+
+      # jklm.preds <- predict.junkle(jklm, dat[c(idx.train, idx.test), ], pathways, NULL,
+      #   FALSE)$preds
+
+
+      message(paste0(i, " make predictions"))
+      jklm.preds <- predict.junkle(jklm, junkle_predict_data, junkle_feature_sets,
+        junkle_kernels, junkle_store_kernels)$preds
+
+      message(paste0(i, " collect results"))
+
+      jklm.preds <- apply(jklm.preds, 1, function(x) colnames(jklm.preds)[which.max(x)])
+
+      confM <- caret::confusionMatrix(factor(jklm.preds, levels = levels(labels)), labels[idx.test])
+
+      # get BACC results
+      if (classification_type == "binary") {
+        # for binary classification, use this
+        bacc <- unname(confM$byClass["Balanced Accuracy"])
+      } else if (classification_type == "multiclass") {
+        # for multiclass classification, use this
+        bacc <- mean(unname(confM$byClass[, "Balanced Accuracy"]))
+      } else {
+        message(paste0(i, " **ERROR** CLASSIFICATION_TYPE must be binary or multiclass. CLASSIFICATION_TYPE=",
+          CLASSIFICATION_TYPE))
+        stopifnot(FALSE)
+      }
+
+
+      save(jklm.preds, confM, bacc, file = paste0(workDir, "/", i, "_junkle_final_model_stats_preds.RData"))
+    }
+    return(bacc)
+  } # end of task
+
+  return(res)
+} # end of worker.f
